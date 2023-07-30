79a96bedab220adef3a4eb3a29e222ae0dae1300
Youesky <135997849+youesky@users.noreply.github.com>
2023-07-30 15:42:15 +0530

    Update products.js

--- products.js
+++ products.js
@@ -1,31 +1,31 @@
-// script.js
-document.addEventListener('DOMContentLoaded', async () => {
-  const itemsPerPage = 4; // Number of products to display per page
-  let currentPage = 1; // Current page number
-  let productsData = []; // Variable to store the product data
+ // script.js
 
-  const fetchProducts = async () => {
-    try {
-      const response = await fetch('products.json');
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      const products = await response.json();
-      return products;
-    } catch (error) {
-      console.error('Error fetching product data:', error);
-      return [];
+const itemsPerPage = 4; // Number of products to display per page
+let currentPage = 1; // Current page number
+let productsData = []; // Variable to store the product data
+
+const fetchProducts = async () => {
+  try {
+    const response = await fetch('products.json');
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
     }
-  };
+    const products = await response.json();
+    return products;
+  } catch (error) {
+    console.error('Error fetching product data:', error);
+    return [];
+  }
+};
 
-  const renderProducts = () => {
-    const productsContainer = document.querySelector(".products");
-    const startIndex = (currentPage - 1) * itemsPerPage;
-    const endIndex = startIndex + itemsPerPage;
-    const currentProducts = productsData.slice(startIndex, endIndex);
+const renderProducts = () => {
+  const productsContainer = document.querySelector(".products");
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const currentProducts = productsData.slice(startIndex, endIndex);
 
-    const output = currentProducts.map(item => `
-      <div class="w3-col l3 s6">
+  const output = currentProducts.map(item => `
+    <div class="w3-col l3 s6">
         <div class="w3-container">
           <div class="w3-display-container">  
             <a href="${item.link}" target="_blank"><img src="${item.image}" alt="${item.alt}" style="width:100%;"></a>
@@ -33,73 +33,36 @@ document.addEventListener('DOMContentLoaded', async () => {
           </div>
         </div><br>
       </div>
-    `).join('');
-    productsContainer.innerHTML = output;
-  };
+  `).join('');
+  productsContainer.innerHTML = output;
+};
 
-  const renderPagination = () => {
-    const totalPages = Math.ceil(productsData.length / itemsPerPage);
+const renderPagination = () => {
+  const totalPages = Math.ceil(productsData.length / itemsPerPage);
 
-    const paginationContainer = document.querySelector(".pagination");
-    paginationContainer.innerHTML = '';
-
-    for (let i = 1; i <= totalPages; i++) {
-      const button = document.createElement('button');
-      button.textContent = i;
-      button.addEventListener('click', () => {
-        currentPage = i;
-        renderProducts();
-        renderPagination();
-      });
-
-      // Add "active" class to the current page button
-      if (i === currentPage) {
-        button.classList.add('active');
-      }
-
-      paginationContainer.appendChild(button);
-    }
-  };
-
-  // Swipe functionality for product frame and pagination
-  const productFrame = document.querySelector(".products");
   const paginationContainer = document.querySelector(".pagination");
+  paginationContainer.innerHTML = '';
 
-  let touchStartX = 0;
-  let touchEndX = 0;
+  for (let i = 1; i <= totalPages; i++) {
+    const button = document.createElement('button');
+    button.textContent = i;
+    button.addEventListener('click', () => {
+      currentPage = i;
+      renderProducts();
+      renderPagination();
+    });
 
-  productFrame.addEventListener('touchstart', handleTouchStart, false);
-  productFrame.addEventListener('touchend', handleTouchEnd, false);
-
-  function handleTouchStart(event) {
-    touchStartX = event.changedTouches[0].screenX;
-  }
-
-  function handleTouchEnd(event) {
-    touchEndX = event.changedTouches[0].screenX;
-    handleSwipe();
-  }
-
-  function handleSwipe() {
-    const swipeThreshold = 50; // Minimum distance for a swipe to be considered
-
-    const swipeDistance = touchEndX - touchStartX;
-
-    if (swipeDistance > swipeThreshold) {
-      // Swipe right (go to previous page)
-      currentPage = Math.max(currentPage - 1, 1);
-    } else if (swipeDistance < -swipeThreshold) {
-      // Swipe left (go to next page)
-      const totalPages = Math.ceil(productsData.length / itemsPerPage);
-      currentPage = Math.min(currentPage + 1, totalPages);
+    // Add "active" class to the current page button
+    if (i === currentPage) {
+      button.classList.add('active');
     }
 
-    renderProducts();
-    renderPagination();
+    paginationContainer.appendChild(button);
   }
+};
 
-  // Fetch products data and render initial products and pagination
+(async () => {
   productsData = await fetchProducts();
   renderProducts();
   renderPagination();
-});
+})();
